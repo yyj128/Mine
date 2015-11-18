f4e70b26cce96e671b7ddea619913666e61dbfac
yyj128 <nyeongjyu@naver.com>
2015-11-19 01:07:08 +0900

    1119_0107

--- express.js
+++ express.js
@@ -1,14 +1,19 @@
-//express 모듈을 불러오기
-var express = require('express');
-
+var express = require("express");
 var app = express();
+var router = express.Router();
+var path = __dirname + '/views/';
 
-// 라우팅 설정 및 Client에 보낼 데이터 설정
-app.get('/', function(req, res){
-	res.sendfile('Login.html');
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
 });
 
-// 포트 설정 및 시작 동작 설정
-app.listen(6180, function(){
-	console.log('Server Start');
+router.get("/",function(req,res){
+  res.sendFile(path + "Login.html");
+});
+
+app.use("/",router);
+
+app.listen(6180,function(){
+  console.log("server start / port 6180");
 });
